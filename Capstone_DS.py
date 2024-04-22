c81b869f95bfaadf77e38ce89244bd7215375152
mukuldesai <144817504+mukuldesai@users.noreply.github.com>
2024-04-22 15:31:23 -0400

    Update Capstone_DS.py

--- Capstone_DS.py
+++ Capstone_DS.py
@@ -34,7 +34,7 @@ pip install streamlit
 # In[7]:
 
 
-import streamlit as st
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
