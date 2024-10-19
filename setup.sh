c79b869642e32f2b4a62d9dd85a023881ede011c
mukuldesai <144817504+mukuldesai@users.noreply.github.com>
2024-10-19 16:16:28 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 apt-get update && apt-get install -y \
     python3-dev \
-    libjpeg-dev \
-    zlib1g-dev \
-    libfreetype6-dev \
-    liblcms2-dev \
-    libtiff5-dev \
-    libwebp-dev \
-    libharfbuzz-dev \
+    libjpeg-dev
+    zlib1g-dev
+    libfreetype6-dev
+    liblcms2-dev
+    libtiff5-dev
+    libwebp-dev
+    libharfbuzz-dev
     libfribidi-dev
+
