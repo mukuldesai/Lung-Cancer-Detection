538f12da8756e7d36863026977e72eef4524097d
mukuldesai <144817504+mukuldesai@users.noreply.github.com>
2024-10-19 12:32:05 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,16 @@ apt-get update && apt-get install -y \
     build-essential \
     python3-dev \
     libjpeg-dev \
-    zlib1g-dev
+    zlib1g-dev \
+    libfreetype6-dev \
+    liblcms2-dev \
+    libtiff5-dev \
+    libopenjp2-7-dev \
+    libwebp-dev \
+    libharfbuzz-dev \
+    libfribidi-dev \
+    libxcb1-dev
 
 # Install Python dependencies
+pip install --upgrade pip  # Make sure pip is updated
 pip install -r requirements.txt
